594d47877bd55cb2f8006cdcb7126a7c14296457
ThbPS <thomas.broussoux@prestashop.com>
2025-06-23 13:03:29 +0200

    fix: file watcher for mac & windows

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
-import * as express from 'express';
-import { Router } from 'express';
+import express, { Router } from 'express';
 import { storeArtworks } from './api/controllers/artwork.controller';
 
 const app = express();
